e138028ac73a2b6db44c608b17ab811e585d82e9
Stephen DuVall <stephen.duvall13@gmail.com>
2018-11-04 10:23:23 -0500

    books array for bible, only the first 8 for testing

---
+++ getBooks.js
@@ -0,0 +1,13 @@
+var books = [
+  'Genesis',
+  'Exodus',
+  'Leviticus',
+  'Numbers',
+  'Deuteronomy',
+  'Joshua',
+  'Judges',
+  'Ruth',
+];
+
+module.exports = books;
+// https://gist.github.com/mastastealth/cd5e12f6c0960408a3ac
